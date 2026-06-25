c944203f3055ec3af272119635c365cb86f383cf
ÄTAGWE ROGER <atagweroger2@gmail.com>
2026-06-25 18:42:44 +0100

    dev commit

--- frontend/lux-sendi/components/services/PackagesCard.tsx
+++ frontend/lux-sendi/components/services/PackagesCard.tsx
@@ -1,10 +1,10 @@
-import { packages } from "@/lib/data/packages";
+import { services } from "@/lib/data/services";
 
 export default function PackageCards() {
   return (
     <section className="pb-24">
       <div className="max-w-6xl mx-auto px-6 grid md:grid-cols-3 gap-8">
-        {packages.map((pkg , index) => (
+        {services.map((pkg , index) => (
           <div
             key={index}
             className="rounded-3xl border p-8 hover:shadow-xl transition"
